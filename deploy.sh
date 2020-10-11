733545b14b5c3782339656f3390c477f531b58dc
nafsadh <khan@nafsadh.com>
2020-10-11 00:26:25 -0700

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:nafsadh/elf-street.git master:gh-pages
+git push -f git@github.com:nafsadh/elf-street.git main:gh-pages
 
 cd -

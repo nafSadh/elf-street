f8eef4e7a71dfc1158dc7909768dce349d9adb6e
nafsadh <khan@nafsadh.com>
2020-10-11 13:00:04 -0700

    stub ETF page

---
+++ pages/etf/_etf.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    XYZ
+    {{ $route.params }}
+    {{ etfId }}
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    etfId() {
+      return this.$route.params.etf
+    },
+    etfData() {
+      const etfJson = require('~/static/etf/' + this.etfId + '.json')
+      return etfJson
+    },
+  },
+}
+</script>

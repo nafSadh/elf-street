4255f35178081ac415377ca7d147e0f4a549526f
nafsadh <khan@nafsadh.com>
2020-10-11 03:01:31 -0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
+  mode: 'spa',
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',

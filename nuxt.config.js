e032de5bad1effa9ceaa805234fb915d6fe1e5ac
nafsadh <khan@nafsadh.com>
2020-10-11 03:07:17 -0700

    nuxt universal

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
-  mode: 'spa',
+  mode: 'universal',
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
